96077890d0896d69a3c5721f9e6ff6d60ee1d8be
gurshaan17 <gurshaansinghd@gmail.com>
2024-06-12 11:28:14 +0530

    routing done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,35 @@
 import route from '../assets/route.png'
 import React from "react";
 
+const NavigateToblog = () => {
+    window.location.href='/blog'
+}
+
+const NavigateToMain = () => {
+    window.location.href='/'
+}
+
+const NavigateToAbout = () => {
+    window.location.href='/about'
+}
+
+const NavigateToContact = () => {
+    window.location.href='/contact'
+}
+
 const Navbar = () => {
     return <div>
         <header className="flex justify-between items-center p-5 bg-gray-200 text-black">
             <div className="flex items-center pl-9">
                 <img src={route} className='h-16 w-16'  />
-                <button  className="text-4xl font-bold pt-5 pb-5">Touring</button>
+                <button onClick={NavigateToMain}  className="text-4xl font-bold pt-5 pb-5">Touring</button>
             </div>
             <nav>
-                <button className="ml-5 text-black hover:underline text-2xl p-6">About Us</button>
+                <button onClick={NavigateToAbout} className="ml-5 text-black hover:underline text-2xl p-6">About Us</button>
                 <button className="ml-5 text-black hover:underline text-2xl p-6">Package</button>
                 <button className="ml-5 text-black hover:underline text-2xl p-6">Gallery</button>
-                <button className="ml-5 text-black hover:underline text-2xl p-6">Blog</button>
-                <button className="ml-5 text-black hover:underline text-2xl p-6">Contact Us</button>
+                <button onClick={NavigateToblog} className="ml-5 text-black hover:underline text-2xl p-6">Blog</button>
+                <button onClick={NavigateToContact} className="ml-5 text-black hover:underline text-2xl p-6">Contact Us</button>
             </nav>
         </header>
     </div>
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import About from "../components/About";
 import Footer from "../components/Footer";
+import InfoCard from "../components/InfoCards";
 import Navbar from "../components/Navbar";
 
 const AboutUs = () => {
     return <div>
         <Navbar/>
         <About/>
+        <InfoCard/>
         <Footer/>
     </div>
 }
